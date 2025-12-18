64771bf9402b5506e4345970148d48604446c4b6
Shivam Kumar <142294240+ShivamK15493@users.noreply.github.com>
2025-12-18 14:59:25 +0530

    Add files via upload

---
+++ Student Grade System/index.js
@@ -0,0 +1,44 @@
+
+function searching()
+{
+    let num = parseInt(document.getElementById("inputBox").value);
+    let result;
+
+   if(num>=91 && num<=100)
+     {
+        result=num+": A+ :"+"Excellent";
+     }
+    else if(num>=81 && num<=90)
+       {
+        result=num+": A :"+"Good";
+       }
+    else if(num>=71 && num<=80)
+    {
+        result=num+": B :"+"Normal";
+    }
+    else if(num>=61 && num<=70)
+    {
+        result=num+": C :"+"Average";
+    }
+    else if(num>=51 && num<=60)
+    {
+        result=num+": D :"+"Poor";
+    }
+    else if(num>=33 && num<=50)
+    {
+        result=num+": E :"+"Very Poor";
+    }
+    else if(num>=0 && num<=33)
+    {
+        result=num+": F :"+"Fail";
+    }
+       else
+       {
+        result="Error";
+       }
+     document.getElementById("res").innerText=result;
+   }
+function forgot()
+{
+    location.reload();
+}
